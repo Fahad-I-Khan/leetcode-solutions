6bcd6c8e40074539e1186221cb117bfa8471d3af
Fahad-I-Khan <khanfahad2105@gmail.com>
2025-02-12 20:02:30 +0530

    More ways to solve

--- Problem 217/main.go
+++ Problem 217/main.go
@@ -1,5 +1,6 @@
 package main
 
+// this one i soleved 
 func containsDuplicate(nums []int) bool {
 	if len(nums) < 2 {
 		return false
@@ -16,3 +17,44 @@ func containsDuplicate(nums []int) bool {
 	return false
 }
 
+//This one from leetcode solutions 
+
+// func containsDuplicate(nums []int) bool {
+//     m := make(map[int]bool, len(nums))
+    
+//     for i := range nums {
+//         if m[nums[i]] {
+//             return true
+//         }
+//         m[nums[i]] = true
+//     }
+//     return false 
+// }
+
+// This solution is from leetcode but without using hashMap
+
+// func containsDuplicate(nums []int) bool {
+//     l := len(nums)
+//     i := 0
+//     sorted := true
+//     for sorted {
+//         for i < l-1 {
+//             if nums[i] <= nums[i+1] {
+//                 if nums[i] == nums[i+1] {
+//                     return true
+//                 }
+//                 i++
+//             } else {
+//                 nums[i],nums[i+1] = nums[i+1],nums[i]
+//                 sorted = false
+//             }
+//         }
+//         if !sorted {
+//             i = 0
+//             sorted = true
+//         } else {
+//             break
+//         } 
+//     }
+//     return false
+// }
