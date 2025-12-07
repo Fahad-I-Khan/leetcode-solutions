c9be470700f6c09a2633490d784459fdc8bf5296
Fahad-I-Khan <khanfahad2105@gmail.com>
2025-12-07 11:33:06 +0530

    One More Way To Solve

--- Problem 242/main.go
+++ Problem 242/main.go
@@ -4,29 +4,48 @@ import (
     "fmt"
 )
 
+// func isAnagram(s string, t string) bool {
+//     if len(s) != len(t) {
+//         return false
+//     }
+    
+//     // Create a frequency map for the characters in the first string
+//     freq := make(map[rune]int)
+    
+//     // Increment counts based on the first string
+//     for _, char := range s {
+//         freq[char]++
+//     }
+    
+//     // Decrement counts based on the second string
+//     for _, char := range t {
+//         freq[char]--
+//         // If a character count goes negative, strings are not anagrams
+//         if freq[char] < 0 {
+//             return false
+//         }
+//     }
+    
+//     // If all counts are zero, then the strings are anagrams
+//     return true
+// }
+
 func isAnagram(s string, t string) bool {
     if len(s) != len(t) {
         return false
     }
-    
-    // Create a frequency map for the characters in the first string
-    freq := make(map[rune]int)
-    
-    // Increment counts based on the first string
-    for _, char := range s {
-        freq[char]++
+    isPresent := make(map[rune]int)
+    for _, ch := range s {
+        isPresent[ch]++
     }
-    
-    // Decrement counts based on the second string
-    for _, char := range t {
-        freq[char]--
-        // If a character count goes negative, strings are not anagrams
-        if freq[char] < 0 {
+    for _, ch := range t {
+        isPresent[ch]--
+    }
+    for _, count := range isPresent {
+        if count != 0 {
             return false
         }
     }
-    
-    // If all counts are zero, then the strings are anagrams
     return true
 }
 
